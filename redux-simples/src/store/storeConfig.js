0288c49089de555a09781e60d4388c04c728a053
Willie Santos <willie_santos@hotmail.com>
2022-10-18 16:21:23 -0300

    att redux-simples

---
+++ redux-simples/src/store/storeConfig.js
@@ -0,0 +1,14 @@
+import { combineReducers, legacy_createStore as createStore } from "redux";
+
+import numerosReducer from './reducers/numeros'
+
+const reducers = combineReducers({
+    numeros: numerosReducer,
+    
+})
+
+function storeConfig() {
+    return createStore(reducers)
+}
+
+export default storeConfig
